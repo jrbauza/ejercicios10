416bbc5cb447a94b193e08c9814345484cecbbd4
Juan Ramis Bauza <ramisjuan1@gmail.com>
2022-09-03 02:15:06 +0200

    Add secure paths and users

--- src/main/java/ob/spring/ejercicios10/Ejercicios10Application.java
+++ src/main/java/ob/spring/ejercicios10/Ejercicios10Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ejercicios10Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable{
 		SpringApplication.run(Ejercicios10Application.class, args);
 	}
 
---
+++ src/main/java/ob/spring/ejercicios10/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package ob.spring.ejercicios10.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("admin").password("123456").roles("ADMIN")
+                .and()
+                .withUser("dev").password("12dev").roles("USER");
+    }
+}
